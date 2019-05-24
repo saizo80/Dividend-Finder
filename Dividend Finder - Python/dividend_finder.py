9f8a7b6d8b80f1070c1c2be1453251dd7949c051
matt8094 <50975890+matt8094@users.noreply.github.com>
2019-05-24 18:27:26 -0500

    Update dividend_finder.py

--- Dividend Finder - Python/dividend_finder.py
+++ Dividend Finder - Python/dividend_finder.py
@@ -1,18 +1,15 @@
-def main():
+def finder():
     divisor = int(1)
     dividend = int(input("Please enter a number. "))
     while divisor <= dividend:
-        if (dividend%divisor == 0):
-            multiplyer = (dividend//divisor)
-            multiplyer = (multiplyer)
-            print (divisor,'x',multiplyer,'=',dividend)
-            #Line 8 was a bitch to figure out#
+        if dividend % divisor == 0:
+            multiplyer = dividend/divisor
+            print (divisor,'x', multiplyer,'=',dividend)
         divisor = divisor + 1
 
 while True:
-    main()
-    if input("Repeat? y/n ") != 'y':
+    finder()
+    print ("Would you like to run again? Y/N: ")
+    choice = input()
+    if 'y' not in choice.lower():
         break
-
-#Coded by Matthew Thornton because he was#
-#too depressed and needed an outlet of creativity#
