aab8ef4df75714ab4cd620749aeb9d9bd76c25c9
matt8094 <50975890+matt8094@users.noreply.github.com>
2019-07-07 18:33:50 -0500

    Update Python

--- Dividend Finder - Python/dividend_finder.py
+++ Dividend Finder - Python/dividend_finder.py
@@ -1,18 +1,35 @@
-def finder():
-    divisor = int(1)
-    dividend = int(input("Please enter a number. "))
-    if dividend == 1:
-        print ('1 x 1 = 1')
-        return
-    while divisor <= dividend:
-        if dividend % divisor == 0:
-            multiplyer = dividend/divisor
-            print (divisor,'x', multiplyer,'=',dividend)
-        divisor = divisor + 1
+# /usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@author: Matthew Thornton
 
-while True:
-    finder()
-    print ("Would you like to run again? Y/N: ")
-    choice = input()
-    if 'y' not in choice.lower():
-        break
+This program takes an input and lists every possible whole number
+multiplication that equals the specified number.
+"""
+
+class Dividend_Finder(object):
+    def __init__(self):
+        self.divisor = 1
+    def main(self, dividend):
+        divisor = self.divisor
+        if dividend == 1:
+            print ('1 x 1 = 1')
+            return
+        while divisor <= dividend:
+            if dividend % divisor == 0:
+                multiplyer = dividend//divisor
+                print (divisor,'x', multiplyer ,'=', dividend)
+                if divisor == multiplyer:
+                    break
+            divisor += 1
+            if divisor == multiplyer:
+                break
+
+if __name__ == '__main__':
+    finder = Dividend_Finder()
+    while True:
+        dividend = input("Please enter a number: ")
+        dividend = int(dividend)
+        finder.main(dividend)
+        if 'y' not in input("Would you like to run again? Y/N: ").lower():
+            break
