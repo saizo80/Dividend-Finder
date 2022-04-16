1d8e7d54568fb6754f69944397bafce6b008e060
matt8094 <github@azerox.net>
2022-04-16 16:00:50 -0500

    rename folders

---
+++ Rust/target/debug/deps/Dividend_Finder-f2efd7c30046dd05.d
@@ -0,0 +1,5 @@
+/Users/olympia/Documents/GitHub/Dividend-Finder/Dividend-Finder-Rust/target/debug/deps/Dividend_Finder-f2efd7c30046dd05: src/main.rs
+
+/Users/olympia/Documents/GitHub/Dividend-Finder/Dividend-Finder-Rust/target/debug/deps/Dividend_Finder-f2efd7c30046dd05.d: src/main.rs
+
+src/main.rs:
---
+++ Rust/target/rls/debug/deps/Dividend_Finder-2cc24c5a4899584f.d
@@ -0,0 +1,5 @@
+/Users/olympia/Documents/GitHub/Dividend-Finder/Dividend-Finder-Rust/target/rls/debug/deps/Dividend_Finder-2cc24c5a4899584f.rmeta: src/main.rs
+
+/Users/olympia/Documents/GitHub/Dividend-Finder/Dividend-Finder-Rust/target/rls/debug/deps/Dividend_Finder-2cc24c5a4899584f.d: src/main.rs
+
+src/main.rs:
