b930a2d0d13f749e2336ca7ef11e2f5179783885
matt8094 <github@azerox.net>
2022-04-16 14:44:05 -0500

    write functions

--- Dividend-Finder-Rust/src/main.rs
+++ Dividend-Finder-Rust/src/main.rs
@@ -1,3 +1,29 @@
+//
+// Dividend Finder
+// Created by Olympia Thornton on April 16 2022
+//
+//
+
+fn digit_checker(s: &String) -> bool {
+    return s.chars().all(char::is_numeric);
+}
+
+fn get_input() -> String {
+    let mut line = String::new();
+    println!("Enter a number: ");
+    std::io::stdin().read_line(&mut line).unwrap();
+    println!();
+    if let Some('\n')=line.chars().next_back() {line.pop();}
+    //if let Some('\r')=line.chars().next_back() {line.pop();}
+    return line;
+}
 fn main() {
-    println!("Hello, world!");
+    let mut number: i32 = 0;
+    let input = get_input();
+    if digit_checker(&input) {
+        number = input.parse().unwrap();
+    }
+    if number != 0 {
+        // do something here
+    }
 }
